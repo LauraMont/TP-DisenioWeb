3857d0443208e0a0c708ea4eb60ccb06a6adff66
joseirojas <joseerojas6@gmail.com>
2023-06-28 20:42:20 -0300

    Formato y JS para formulario de contacto

--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -29,3 +29,50 @@ $(document).ready(function() {
     });
   });
   
+// Función para validar el formulario de contacto
+function validateContactForm() {
+  const form = document.getElementById('contactForm');
+  const nameInput = document.getElementById('name');
+  const phoneInput = document.getElementById('phone');
+  const emailInput = document.getElementById('email');
+  const messageInput = document.getElementById('message');
+
+  // Validar que el campo de nombre y apellido no esté vacío
+  if (nameInput.value.trim() === '') {
+    alert('Por favor, ingresa tu nombre y apellido');
+    return false;
+  }
+
+  // Validar que el campo de teléfono contenga solo números
+  if (!/^[0-9]+$/.test(phoneInput.value)) {
+    alert('Por favor, ingresa un número de teléfono válido');
+    return false;
+  }
+
+  // Validar que el campo de correo electrónico tenga el formato adecuado
+  if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailInput.value)) {
+    alert('Por favor, ingresa una dirección de correo electrónico válida');
+    return false;
+  }
+
+  // Validar que el campo de mensaje no esté vacío
+  if (messageInput.value.trim() === '') {
+    alert('Por favor, ingresa tu mensaje');
+    return false;
+  }
+
+  // Si todos los campos son válidos, el formulario se envía
+  return true;
+}
+
+// Escuchar el evento de envío del formulario
+const contactForm = document.getElementById('contactForm');
+contactForm.addEventListener('submit', function (event) {
+  event.preventDefault(); // Evitar el envío del formulario por defecto
+  if (validateContactForm()) {
+    // Aquí puedes agregar el código para enviar el formulario
+    // por ejemplo, utilizando AJAX o enviándolo a un servidor
+    alert('Formulario enviado correctamente');
+    contactForm.reset(); // Limpiar el formulario después de enviarlo
+  }
+});
